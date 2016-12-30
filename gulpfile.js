950508d23ff82ec4079f275f0a0e92f0ca0521d5
vishal18-gupta <vishal18.gupta@gmail.com>
2016-12-30 15:50:09 +0530

    browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,41 @@
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
 
-gulp.task('default', function() {
+
+gulp.task('inject', function() {
+
+    var wiredep = require('wiredep').stream;
+    var options = {
+        bowerJson: require('./bower.json'),
+        directory: 'public/lib',
+        ignorePath: '../../public/'
+    }
+
+    var gulpInject = require('gulp-inject');
+    var sources = gulp.src(['./public/css/*.css', './public/js/*.js'], { read: false });
+    var optionInject = {
+        ignorePath: '/public/'
+    }
+
+    gulp.src('./src/views/*.html')
+        .pipe(wiredep(options))
+        .pipe(gulpInject(sources, optionInject))
+        .pipe(gulp.dest('./src/views'));
+
+});
+
+var browserSync = require('browser-sync').create();
+
+// Static Server + watching scss/html files
+gulp.task('serve', function() {
+
+    browserSync.init({
+        server: "./src/views"
+    });
+    gulp.watch("./src/views/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default', ['inject', 'serve'], function() {
     nodemon({
             script: 'app.js',
             ext: 'js',
